45a5f470f934dcb637322ebc7879858ab7cfd598
ralphzera <rafaelprimo09@gmail.com>
2026-02-22 14:50:17 -0300

    Troca multipla de moedas e conversoes alem do display de conversao

--- scripts.js
+++ scripts.js
@@ -1,102 +1,186 @@
 
 
 
-function trocadisplay() {
-    const moedaescolhida = document.getElementById("moedas").value;
-    const bandeira = document.querySelector("#bandeira-que-muda")
-    const textoemcima = document.querySelector("#texto-que-muda")
-    const resultado = document.querySelector("#resultado")
+function trocadisplayEmbaixo() {
+    const moedaescolhidaEmbaixo = document.querySelector("#moedas-embaixo").value;
+    const bandeiraEmbaixo = document.querySelector("#bandeira-da-moeda-convertida")
+    const textoembaixo = document.querySelector("#texto-embaixo")
+    const resultadoEmbaixo = document.querySelector("#resultado-embaixo")
 
-    if (moedaescolhida == "libra") {
-        bandeira.src = "assets/libra-1.png"
-        textoemcima.textContent = "Libra"
-        resultado.innerHTML = "£ 0.00"
-
-    }
-    if (moedaescolhida == "euro") {
-        bandeira.src = "assets/euro-1.png"
-        textoemcima.textContent = "Euro"
-        resultado.innerHTML = "€  0.00"
+    if (moedaescolhidaEmbaixo == "libra") {
+        bandeiraEmbaixo.src = "assets/libra-1.png"
+        textoembaixo.textContent = "Libra"
+        resultadoEmbaixo.innerHTML = "£ 0.00"
 
     }
-    if (moedaescolhida == "dolar") {
-        bandeira.src = "assets/dolar-1.png"
-        textoemcima.textContent = "Dólar Americano"
-        resultado.innerHTML = "US$ 0.00"
+    else if (moedaescolhidaEmbaixo == "euro") {
+        bandeiraEmbaixo.src = "assets/euro-1.png"
+        textoembaixo.textContent = "Euro"
+        resultadoEmbaixo.innerHTML = "€  0.00"
 
     }
-    if (moedaescolhida == "bitcoin") {
-        bandeira.src = "assets/bitcoin-1.png"
-        textoemcima.textContent = "Bitcoin"
-        resultado.innerHTML = "BTC 0.00"
+    else if (moedaescolhidaEmbaixo == "dolar") {
+        bandeiraEmbaixo.src = "assets/dolar-1.png"
+        textoembaixo.textContent = "Dólar Americano"
+        resultadoEmbaixo.innerHTML = "US$ 0.00"
+
+    }
+    else if (moedaescolhidaEmbaixo == "bitcoin") {
+        bandeiraEmbaixo.src = "assets/bitcoin-1.png"
+        textoembaixo.textContent = "Bitcoin"
+        resultadoEmbaixo.innerHTML = "BTC 0.00"
+
+    }
+    else if (moedaescolhidaEmbaixo == "real") {
+        bandeiraEmbaixo.src = "assets/bandeira-circulo-brasil.png"
+        textoembaixo.textContent = "Real"
+        resultadoEmbaixo.innerHTML = "R$ 0.00"
 
     }
 
-    return moedaescolhida
 
 }
+
+function trocadisplayEmcima(){
+        
+    const moedaescolhidaEmcima = document.querySelector("#moedas-emcima").value;
+    const bandeiraEmcima = document.querySelector("#bandeira-da-moeda-a-ser-convertida")
+    const textoemcima = document.querySelector("#texto-emcima")
+    const resultadoEmcima = document.querySelector("#resultado-emcima")
+
+
+
+
+
+    
+    if (moedaescolhidaEmcima == "libra") {
+        bandeiraEmcima.src = "assets/libra-1.png"
+        textoemcima.textContent = "Libra"
+        resultadoEmcima.innerHTML = "£ 0.00"
+
+    }
+    if (moedaescolhidaEmcima == "euro") {
+        bandeiraEmcima.src = "assets/euro-1.png"
+        textoemcima.textContent = "Euro"
+        resultadoEmcima.innerHTML = "€  0.00"
+
+    }
+    if (moedaescolhidaEmcima == "dolar") {
+        bandeiraEmcima.src = "assets/dolar-1.png"
+        textoemcima.textContent = "Dólar Americano"
+        resultadoEmcima.innerHTML = "US$ 0.00"
+
+    }
+    if (moedaescolhidaEmcima == "bitcoin") {
+        bandeiraEmcima.src = "assets/bitcoin-1.png"
+        textoemcima.textContent = "Bitcoin"
+        resultadoEmcima.innerHTML = "BTC 0.00"
+
+    }
+    if (moedaescolhidaEmcima == "real") {
+        bandeiraEmcima.src = "assets/bandeira-circulo-brasil.png"
+        textoemcima.textContent = "Real"
+        resultadoEmcima.innerHTML = "R$ 0.00"
+
+    }
+
+}
+
+
+
 function atualizaDisplayReal() {
     const valorInput = document.querySelector("#input-valores").value
-    const novoValor = document.querySelector("#valor-real")
-    const real = document.querySelector("#real")
-    const numero = Number(valorInput)
+    const novoValor = document.querySelector("#resultado-emcima")
+    const moedaOrigem = document.querySelector("#moedas-emcima").value
 
-    novoValor.innerText = "R$ " + valorInput
-    if (numero === 1) {
-        real.textContent = "Real"
+
+    if (moedaOrigem === "dolar") {
+       novoValor.innerText = "US$ " + valorInput
     }
-    else {
-        real.textContent = "Reais"
+    if (moedaOrigem === "euro") {
+        novoValor.innerText = "€ " + valorInput
+    }
+    if (moedaOrigem === "libra") {
+        novoValor.innerText = "£ " + valorInput
+    }
+    if (moedaOrigem === "real") {
+        novoValor.innerText = "R$ " + valorInput
+    }
+    if (moedaOrigem === "bitcoin") {
+        novoValor.innerText = "BTC " + valorInput
     }
 
-    return novoValor
+    
 
 }
 
 function converter() {
     const valor = Number(document.querySelector("#input-valores").value)
-    const moeda = document.getElementById("moedas").value
+    const moedaOrigem = document.querySelector("#moedas-emcima").value
+    const moedaDestino = document.querySelector("#moedas-embaixo").value
 
+    let taxaOrigem
+    let taxaDestino
 
-    let taxa
-
-    if (moeda === "dolar") {
-        taxa = 5.2
+    // TAXA ORIGEM
+    if (moedaOrigem === "dolar") {
+        taxaOrigem = 5.2
+    }
+    if (moedaOrigem === "euro") {
+        taxaOrigem = 5.6
+    }
+    if (moedaOrigem === "libra") {
+        taxaOrigem = 6.4
+    }
+    if (moedaOrigem === "bitcoin") {
+        taxaOrigem = 300000
+    }
+    if (moedaOrigem === "real") {
+        taxaOrigem = 1
     }
 
-    if (moeda === "euro") {
-        taxa = 5.6
+
+
+    // TAXA DESTINO
+    if (moedaDestino === "dolar") {
+        taxaDestino = 5.2
+    }
+    if (moedaDestino === "euro") {
+        taxaDestino = 5.6
+    }
+    if (moedaDestino === "libra") {
+        taxaDestino = 6.4
+    }
+    if (moedaDestino === "bitcoin") {
+        taxaDestino = 300000
+    }
+    if (moedaDestino === "real") {
+        taxaDestino = 1
     }
 
-    if (moeda === "libra") {
-        taxa = 6.4
-    }
-
-    if (moeda === "bitcoin") {
-        taxa = 300000
-    }
-    const resultado = valor / taxa
-
-    let resultadoFormatado
-    if (moeda === "dolar") {
-        resultadoFormatado = "US$ " + resultado.toFixed(2)
-    }
-
-    if (moeda === "euro") {
-        resultadoFormatado = "€ " + resultado.toFixed(2)
-    }
-
-    if (moeda === "libra") {
-        resultadoFormatado = "£ " + resultado.toFixed(2)
-    }
-
-    if (moeda === "bitcoin") {
-        resultadoFormatado = "BTC " + resultado.toFixed(8)
-    }
+    const resultado = (valor*taxaOrigem) / taxaDestino
 
     
 
-    document.querySelector("#resultado").innerText = resultadoFormatado
+    let resultadoFormatado
+
+    if (moedaDestino === "dolar") {
+        resultadoFormatado = "US$ " + resultado.toFixed(2)
+    }
+    if (moedaDestino === "euro") {
+        resultadoFormatado = "€ " + resultado.toFixed(2)
+    }
+    if (moedaDestino === "libra") {
+        resultadoFormatado = "£ " + resultado.toFixed(2)
+    }
+    if (moedaDestino === "real") {
+        resultadoFormatado = "R$ " + resultado.toFixed(2)
+    }
+    if (moedaDestino === "bitcoin") {
+        resultadoFormatado = "BTC " + resultado.toFixed(8)
+    }
+
+    document.querySelector("#resultado-embaixo").innerText = resultadoFormatado
 }
 
 
